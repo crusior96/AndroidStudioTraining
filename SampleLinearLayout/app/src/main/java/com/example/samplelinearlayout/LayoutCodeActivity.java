419802484033acaf999374f22b416b5327d7d48d
박종혁 <crusior96@gmail.com>
2021-02-21 01:50:27 +0900

    LinearLayout에 대한 Training

---
+++ SampleLinearLayout/app/src/main/java/com/example/samplelinearlayout/LayoutCodeActivity.java
@@ -0,0 +1,28 @@
+package com.example.samplelinearlayout;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LayoutCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout mainLayout = new LinearLayout(this);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+
+        Button button01 = new Button(this);
+        button01.setText("Button 01");
+        button01.setLayoutParams(params);
+        mainLayout.addView(button01);
+
+        setContentView(mainLayout);
+    }
+}
