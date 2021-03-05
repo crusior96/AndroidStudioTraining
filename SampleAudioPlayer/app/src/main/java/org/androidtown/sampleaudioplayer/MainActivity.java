cb5dddd35bc3a5fd523e57bea532d9377de01f1e
박종혁 <crusior96@gmail.com>
2021-03-05 17:13:27 +0900

    안드로이드 내에 있는 Audio 재생 기능에 관한 Training

---
+++ SampleAudioPlayer/app/src/main/java/org/androidtown/sampleaudioplayer/MainActivity.java
@@ -0,0 +1,87 @@
+package org.androidtown.sampleaudioplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    static final String AUDIO_URL = "http://sites.google.com/site/ubiaccessmobile/sample_audio.amr";
+    private MediaPlayer mediaPlayer;
+    private int playbackPosition = 0;
+
+    Button playBtn;
+    Button pauseBtn;
+    Button restartBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playBtn = (Button) findViewById(R.id.playBtn);
+        pauseBtn = (Button) findViewById(R.id.pauseBtn);
+        restartBtn = (Button) findViewById(R.id.restartBtn);
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                try {
+                    playAudio(AUDIO_URL);
+                    Toast.makeText(getApplicationContext(), "음악 파일 재생 시작됨.",
+                            Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (mediaPlayer != null) {
+                    playbackPosition = mediaPlayer.getCurrentPosition();
+                    mediaPlayer.pause();
+                    Toast.makeText(getApplicationContext(), "음악 파일 재생 중지됨.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        restartBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                    mediaPlayer.seekTo(playbackPosition);
+                    Toast.makeText(getApplicationContext(), "음악 파일 재생 재시작됨.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void playAudio(String url) throws Exception{
+        killMediaPlayer();
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setDataSource(url);
+        mediaPlayer.prepare();
+        mediaPlayer.start();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        killMediaPlayer();
+    }
+
+    private void killMediaPlayer(){
+        if(mediaPlayer!=null){
+            try{
+                mediaPlayer.release();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
